844d20cef42c5c1dc91145ddcddab527094265b9
kakakaya <kakakaya+git@gmail.com>
2019-12-19 23:09:32 +0900

    Run yapf

--- read.py
+++ read.py
@@ -12,7 +12,7 @@ import key
 import cv2
 import pyzbar.pyzbar as pyzbar
 import PIL.Image
-import pathlib 
+import pathlib
 import path
 
 SLACK_BOT_TOKEN = key.SLACK_BOT_TOKEN
@@ -23,35 +23,35 @@ slackch = key.slackch
 
 client = slack.WebClient(token=SLACK_BOT_TOKEN)
 
+
 def SearchImage():
-  homeDir = expanduser('~')
-  imageDir = homeDir + '\\Pictures\\Camera Roll'
-  imageList = os.listdir(imageDir)
-  return (imageDir + '\\' + imageList[-1])
+    homeDir = expanduser('~')
+    imageDir = homeDir + '\\Pictures\\Camera Roll'
+    imageList = os.listdir(imageDir)
+    return (imageDir + '\\' + imageList[-1])
+
 
 def QRreade(image):
-  readResult = decode(Image.open(image))
-  if (readResult != []):
-    return readResult
-  else:
-    print('QRコードを検出できませんでした')
-    exit()
+    readResult = decode(Image.open(image))
+    if (readResult != []):
+        return readResult
+    else:
+        print('QRコードを検出できませんでした')
+        exit()
+
 
 def get_shortenURL(longUrl):
     url = 'https://api-ssl.bitly.com/v3/shorten'
     access_token = aat
-    query = {
-            'access_token': access_token,
-            'longurl':longUrl
-            }
-    r = requests.get(url,params=query).json()
+    query = {'access_token': access_token, 'longurl': longUrl}
+    r = requests.get(url, params=query).json()
     return r
 
+
 def post():
-    response = client.chat_postMessage(
-    channel=slackch,text=card
-    )
-    
+    response = client.chat_postMessage(channel=slackch, text=card)
+
+
 print("アイカツQRコードSlack送信システム")
 print('該当の画像があるパスを入れてください')
 print("QRを読み取る場合はQR-Readと入れてください")
@@ -65,13 +65,12 @@ while True:
     except UnicodeDecodeError:
         print("ファジングしようとするなあ！！！！！！！！！！！！！！！！")
         sys.exit()
-        
+
     path = path.strip()
 
     if "exit" in path:
         exit()
 
-        
     domein = pathlib.Path(path)
     domein = domein.suffix.lower()
     if_suffix = ['.jpg', '.png', '.bmp', '.tif', '.jpeg']
@@ -90,53 +89,53 @@ while True:
         except EOFError:
             print("不正な文字列を入れてませんか？？？")
             sys.exit()
-        if retry.lower() in y:   
+        if retry.lower() in y:
             continue
         elif retry.lower() in n:
             break
         else:
             print("リトライしてください")
             continue
-    
+
     if "QR" in path:
-            window_name = "main"
-            cap = cv2.VideoCapture(0)
-            cap.set(3, 1280)
-            cap.set(4, 720)
-            cap.set(5, 15)
-            cv2.namedWindow(window_name)
-        
-            while True:
-                ret, flame = cap.read()
-                cv2.imshow(window_name, flame)
-                tresh = 100
-                max_pixel = 255
-                ret, flame = cv2.threshold(flame, tresh, max_pixel, cv2.THRESH_BINARY)
-                qr_result = pyzbar.decode(flame)
-                if qr_result != []:
-                    path = qr_result[0][0].decode('utf-8', 'ignore')
-                    print(path)
-                    if "http://dcd.sc/" not in path and "http://aikatsu.com/qr/id=" in path and "AK" in path:
-                        print("アイカツ以外のカードは読み込めません。悪しからず。")
-                        sys.exit()
-                    break
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-    
+        window_name = "main"
+        cap = cv2.VideoCapture(0)
+        cap.set(3, 1280)
+        cap.set(4, 720)
+        cap.set(5, 15)
+        cv2.namedWindow(window_name)
+
+        while True:
+            ret, flame = cap.read()
+            cv2.imshow(window_name, flame)
+            tresh = 100
+            max_pixel = 255
+            ret, flame = cv2.threshold(flame, tresh, max_pixel, cv2.THRESH_BINARY)
+            qr_result = pyzbar.decode(flame)
+            if qr_result != []:
+                path = qr_result[0][0].decode('utf-8', 'ignore')
+                print(path)
+                if "http://dcd.sc/" not in path and "http://aikatsu.com/qr/id=" in path and "AK" in path:
+                    print("アイカツ以外のカードは読み込めません。悪しからず。")
+                    sys.exit()
+                break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
     if "http://aikatsu.com/qr/id=" in path:
         print("旧カツのカードは対応していません。別のカードを読み込んでください。")
         print("該当の画像を入れてください")
         print("終了する場合はexitまたはCtrl+Dでお願いします")
         path = "none"
         continue
-    
+
     if "AK" in path:
         print("旧カツのカードは対応していません。別のカードを読み込んでください。")
         print("該当の画像を入れてください")
         print("終了する場合はexitまたはCtrl+Dでお願いします")
         path = "none"
         continue
-        
+
     cv2.destroyAllWindows()
 
     if "http://dcd.sc/" not in path:
@@ -153,7 +152,7 @@ while True:
             except UnicodeDecodeError:
                 print("ファジングしようとするなあ！！！！！！！！！！！！！！！！")
                 sys.exit()
-            if retry.lower() in y:   
+            if retry.lower() in y:
                 continue
             elif retry.lower() in n:
                 break
@@ -164,13 +163,13 @@ while True:
             print("画像ファイルに見せかけた不正なファイルを読み込ませないでください")
             print("強制終了します")
             sys.exit()
-            
+
         path = read[0][0].decode('utf-8', 'ignore')
 
     path = get_shortenURL(path)
 
     print(path)
-    
+
     try:
         card = path['data']['url']
     except TypeError:
@@ -179,14 +178,14 @@ while True:
         print("終了する場合はexitまたはCtrl+Dでお願いします")
         path = "none"
         continue
-        
+
     post()
-    
+
     card = "none"
-    
+
     image = "none"
-    
+
     path = "none"
-    
+
     print("該当の画像を入れてください")
     print("終了する場合はexitまたはCtrl+Dでお願いします")
